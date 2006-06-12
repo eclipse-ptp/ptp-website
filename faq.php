4321c1ef0bf7d5a9347a343c83e322eb2117919d
btibbitts <btibbitts>
2006-06-12 03:11:47 +0000

    different div

--- faq.php
+++ faq.php
@@ -30,7 +30,7 @@
 	# Paste your HTML content between the EOHTML markers!	
 	$html = <<<EOHTML
 <h1>$pageTitle</h1>
-<div class="homeitem3col">
+<div id="maincontent">
 			<h3>This is a wide column</h3>
  <ul>
         <li><a href="#faq1">Q1: How do I download PTP?</a></li>
