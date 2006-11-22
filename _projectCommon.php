8f12025b1f94bd2fd1a18c1c55853a155ede67f7
gwatson <gwatson>
2006-11-22 08:17:07 +0000

    try standard nav

--- _projectCommon.php
+++ _projectCommon.php
@@ -6,16 +6,20 @@
 	# Optional: defaults to system theme 
 	$theme = "";
 
+	require_once($_SERVER['DOCUMENT_ROOT'] . "/projects/common/project-info.class.php");
+	$projectInfo = new ProjectInfo("technology.ptp");
+	$projectInfo->generate_common_nav( $Nav );
+	
 	# Define your project-wide Nav bars here.
 	# Format is Link text, link URL (can be http://www.someothersite.com/), target (_self, _blank), level (1, 2 or 3)
 	# these are optional
-	$Nav->addNavSeparator("PTP Home", 	"/ptp/index.php");
-	$Nav->addCustomNav("Downloads", 		"/ptp/downloads.php", 	"_self", 2);
+	#$Nav->addNavSeparator("PTP Home", 	"/ptp/index.php");
+	#$Nav->addCustomNav("Downloads", 		"/ptp/downloads.php", 	"_self", 2);
 	$Nav->addCustomNav("Builds", 		"/ptp/builds.php", 	"_self", 2);
-	$Nav->addCustomNav("Installation", 		"/ptp/install.php", 		"_self", 2);
+	#$Nav->addCustomNav("Installation", 		"/ptp/install.php", 		"_self", 2);
 	$Nav->addCustomNav("FAQ", 				"/ptp/faq.php", 			"_self", 2);
 	$Nav->addCustomNav("Wiki", 				"http://wiki.eclipse.org/index.php/Parallel_Tools_Platform", "_self", 2);
-	$Nav->addCustomNav("Documentation", 	"/ptp/doc.php", 			"_self", 2);
-	$Nav->addCustomNav("Mailing Lists",	"https://dev.eclipse.org/mailman/listinfo", "_self", 2);
+	#$Nav->addCustomNav("Documentation", 	"/ptp/doc.php", 			"_self", 2);
+	#$Nav->addCustomNav("Mailing Lists",	"https://dev.eclipse.org/mailman/listinfo", "_self", 2);
 
 ?>
