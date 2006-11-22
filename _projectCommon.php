b5f66cc7e0ea04182fffc84bf2be3f644d9d531d
gwatson <gwatson>
2006-11-22 08:23:03 +0000

    updated

--- _projectCommon.php
+++ _projectCommon.php
@@ -8,17 +8,28 @@
 
 	require_once($_SERVER['DOCUMENT_ROOT'] . "/projects/common/project-info.class.php");
 	$projectInfo = new ProjectInfo("technology.ptp");
-	$projectInfo->generate_common_nav( $Nav );
 	
+	function add2users() {
+		global $Nav;
+		$Nav->addCustomNav("Builds", "/ptp/builds.php", "_self", 2);
+		$Nav->addCustomNav("FAQ", "/ptp/faq.php", "_self", 2);
+	}   
+	function add2contributors() {
+		global $Nav;
+		$Nav->addCustomNav("Wiki", "http://wiki.eclipse.org/index.php/Parallel_Tools_Platform", "_self", 2);
+	}
+	
+	$projectInfo->generate_common_nav( $Nav, "add2users", NULL, "add2contributors");
+		
 	# Define your project-wide Nav bars here.
 	# Format is Link text, link URL (can be http://www.someothersite.com/), target (_self, _blank), level (1, 2 or 3)
 	# these are optional
 	#$Nav->addNavSeparator("PTP Home", 	"/ptp/index.php");
 	#$Nav->addCustomNav("Downloads", 		"/ptp/downloads.php", 	"_self", 2);
-	$Nav->addCustomNav("Builds", 		"/ptp/builds.php", 	"_self", 2);
+	#$Nav->addCustomNav("Builds", 		"/ptp/builds.php", 	"_self", 2);
 	#$Nav->addCustomNav("Installation", 		"/ptp/install.php", 		"_self", 2);
-	$Nav->addCustomNav("FAQ", 				"/ptp/faq.php", 			"_self", 2);
-	$Nav->addCustomNav("Wiki", 				"http://wiki.eclipse.org/index.php/Parallel_Tools_Platform", "_self", 2);
+	#$Nav->addCustomNav("FAQ", 				"/ptp/faq.php", 			"_self", 2);
+	#$Nav->addCustomNav("Wiki", 				"http://wiki.eclipse.org/index.php/Parallel_Tools_Platform", "_self", 2);
 	#$Nav->addCustomNav("Documentation", 	"/ptp/doc.php", 			"_self", 2);
 	#$Nav->addCustomNav("Mailing Lists",	"https://dev.eclipse.org/mailman/listinfo", "_self", 2);
 
