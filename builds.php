49f483210f441cf9580735639bfd4b488b274666
btibbitts <btibbitts>
2006-12-13 19:09:40 +0000

    fix html formatting

--- builds.php
+++ builds.php
@@ -74,15 +74,14 @@
           org.eclipse.ptp.pldt-1.1.0-I200612131333.zip</a>
           <p>More info when cannot find artifacts; finds artifacts altered by preprocessor defines; MPI constants now found again; 
           remembers selection in navigator better.  Hopefully one more build will come on Friday.
-          </ul>
-          <p>&nbsp;
+          
         <li>18 July 2006:
           <a href="http://www.eclipse.org/downloads/download.php?file=/technology/ptp/builds/org.eclipse.ptp.pldt-1.0.0-I200607181525.zip" >
           org.eclipse.ptp.pldt-1.0.0-I200607181525.zip</a>
           <p>New project wizard now correctly includes MPI page; help corrected,
           misc. build problems corrected.
-          </ul>
-          <p>&nbsp;
+        </ul>
+        <p>&nbsp;
           
      </div>   
 </div>
