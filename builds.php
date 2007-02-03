1d32131c873e1dd1c5876a9f281ee2d965a3573c
gwatson <gwatson>
2007-02-03 16:59:18 +0000

    I200702030940 build

--- builds.php
+++ builds.php
@@ -45,6 +45,11 @@
      <p>
         <ul>
 
+       <li>3 February 2007: 
+          <a href="http://www.eclipse.org/downloads/download.php?file=/tools/ptp/builds/org.eclipse.ptp-1.1.0-I200702030940.tar.gz" >
+          
+          org.eclipse.ptp-1.1.0-I200702030940.tar.gz</a>
+          
        <li>28 January 2007: 
           <a href="http://www.eclipse.org/downloads/download.php?file=/tools/ptp/builds/org.eclipse.ptp-1.1.0-I200701282100.tar.gz" >
           
