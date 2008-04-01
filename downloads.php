394a74ef6815a6f7c3faf37a4ec860b2bdb3e45b
gwatson <gwatson>
2008-04-01 14:43:08 +0000

    Updated.

--- downloads.php
+++ downloads.php
@@ -38,22 +38,19 @@
       and instructions on building and installing PTP Release 2.0.  
       <p>For complete instructions see also 
       <a href="docs/install.html">Installing Eclipse and PTP Release 2.0.x</a> which includes how to install CDT.
-	<p>PTP 2.0 can be installed from an update site or download of tar/zip files,
-     but  requires an additional installation
-      process over what  normal Eclipse plugins require,</i> so please read 
-<a href="http://wiki.eclipse.org/PTP/release_notes/2.0">the release notes</a> carefully.</p>
-<p>Two alternatives for installation are:
+	<p>The preferred method for installing PTP 2.0 is from an update site, but it can also be downloaded as tar/zip files.</p>
+<p>The two alternatives for installation are:
 <ul>
-<li>To install via the update site: see <a href="http://download.eclipse.org/tools/ptp/releases/2.0/">the PTP 2.0 update site</a>, or
+<li>To install via the update site: see <a href="http://download.eclipse.org/tools/ptp/releases/2.0/">the PTP 2.0 update site</a><p><br></p>
 </li>
     <li>Download: The files below contain the current release of PTP. 
         The PLDT component can be installed independently of PTP Core.
 </li>
 </ul>
     </p>
-     
+
   <div class="homeitem3col">
-  <h3>PTP Core</h3>
+  <h3>PTP Core (includes resource managers and remote tools)</h3>
   <table>
   <tr>
     <td align=left valign=top >
@@ -69,7 +66,27 @@
         </tr>
 
       </table>   
-      </div>
+  </div>
+      
+  <div class="homeitem3col">
+  <h3>PTP Core + RSE (as above but includes support for RSE)</h3>
+  <table>
+  <tr>
+    <td align=left valign=top >
+        <tr bgcolor="#CCCCCC">
+          <th width="34%"  scope="col"><div align="left">Platform</div></th>
+          <th width="10%"  scope="col"><div align="left">Release</div></th>
+          <th width="56%"  scope="col"><div align="left">Download</div></th>
+        </tr>
+        <tr>
+          <td>All</td>
+          <td><div align="left">2.0</div></td>
+          <td><div align="left"><a href="http://www.eclipse.org/downloads/download.php?file=/tools/ptp/releases/2.0/org.eclipse.ptp.rse-2.0.0.tar.gz">org.eclipse.ptp.rse-2.0.0.tar.gz</a></div></td>
+        </tr>
+
+      </table>   
+  </div>
+	  
   <div class="homeitem3col">
   <h3>Parallel Language Development Tools</h3>
      <table>
@@ -94,6 +111,26 @@
         </tr>
       </table>  
 </div>
+
+  <div class="homeitem3col">
+  <h3>Performance Tools Framework (includes support for TAU)</h3>
+  <table>
+  <tr>
+    <td align=left valign=top >
+        <tr bgcolor="#CCCCCC">
+          <th width="34%"  scope="col"><div align="left">Platform</div></th>
+          <th width="10%"  scope="col"><div align="left">Release</div></th>
+          <th width="56%"  scope="col"><div align="left">Download</div></th>
+        </tr>
+        <tr>
+          <td>All</td>
+          <td><div align="left">2.0</div></td>
+          <td><div align="left"><a href="http://www.eclipse.org/downloads/download.php?file=/tools/ptp/releases/2.0/org.eclipse.ptp.perf-2.0.0.tar.gz">org.eclipse.ptp.perf-2.0.0.tar.gz</a></div></td>
+        </tr>
+
+      </table>   
+  </div>
+  
 <div class="homeitem3col">
 <h3>Previous releases</h3>
 <p>The <a href="downloads_1.1.1.php">Release 1.1.1</a> download information is also available.
