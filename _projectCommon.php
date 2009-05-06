8a4ab028c03ac80e44583845d5b95da23f2dc061
gwatson <gwatson>
2009-05-06 19:20:58 +0000

    Fix left hand menu

--- _projectCommon.php
+++ _projectCommon.php
@@ -13,11 +13,15 @@
 		global $Nav;
 		#$Nav->addCustomNav("Mailing Lists", "/mail/index_project.php", "_self", 2);
 		#$Nav->addCustomNav("FAQ", "/ptp/faq.php", "_self", 2);
+		$Nav->addCustomNav("Documentation", "/ptp/doc.php", 		"_self", 2);
+		$Nav->addCustomNav("FAQ", 			"http://wiki.eclipse.org/PTP/FAQ", 		"_self", 2);
+		$Nav->addCustomNav("Mailing Lists",	"http://eclipse.org/ptp/mailing_lists.php", "_self", 2);
 	}   
 	function add2contributors() {
 		global $Nav;
-		#$Nav->addCustomNav("Builds", "/ptp/builds.php", "_self", 2);
-		#$Nav->addCustomNav("Wiki", "http://wiki.eclipse.org/index.php/Parallel_Tools_Platform", "_self", 2);
+		$Nav->addCustomNav("Builds", "/ptp/builds.php", "_self", 2);
+		$Nav->addCustomNav("Source Code",	"http://dev.eclipse.org/viewsvn/index.cgi/org.eclipse.ptp/?root=Tools_Project", "_self", 2);
+		$Nav->addCustomNav("Wiki", "http://wiki.eclipse.org/PTP", "_self", 2);
 	}
 	
 	$projectInfo->generate_common_nav( $Nav, "add2users", NULL, "add2contributors");
@@ -26,11 +30,7 @@
 	# Format is Link text, link URL (can be http://www.someothersite.com/), target (_self, _blank), level (1, 2 or 3)
 	# these are optional
 	$Nav->addNavSeparator("PTP Home", 	"/ptp/index.php");
-	$Nav->addCustomNav("Downloads", 	"/ptp/downloads.php", 	"_self", 2);
-	$Nav->addCustomNav("Builds", 		"http://wiki.eclipse.org/PTP/builds", 		"_self", 2);
-	$Nav->addCustomNav("FAQ", 			"http://wiki.eclipse.org/PTP/FAQ", 		"_self", 2);
-	$Nav->addCustomNav("Wiki", 			"http://wiki.eclipse.org/PTP", "_self", 2);
-	$Nav->addCustomNav("Documentation", "/ptp/doc.php", 		"_self", 2);
-	$Nav->addCustomNav("Mailing Lists",	"http://eclipse.org/ptp/mailing_lists.php", "_self", 2);
+	#$Nav->addCustomNav("Builds", 		"http://wiki.eclipse.org/PTP/builds", 		"_self", 2);
+	#$Nav->addCustomNav("Wiki", 			"http://wiki.eclipse.org/PTP", "_self", 2);
 
 ?>
