ce32ae9a955dfa9c16d37472dd1e9da84ae737b9
gwatson <gwatson>
2009-12-05 02:37:03 +0000

    Updated for 3.0

--- index.php
+++ index.php
@@ -53,6 +53,11 @@
 		<h3>What&apos;s New</h3>
 		<p>Note: the <a href="http://wiki.eclipse.org/PTP">PTP Wiki</a> usually contains the most recent information.
 		<ul>
+		<li>December 4, 2009: PTP 3.0 released! This version contains numerous
+		enhancements and bug fixes. A list of the new
+		features is available from <a href="http://wiki.eclipse.org/PTP/new_and_noteworthy/3.0.0">here</a>.
+		See the <a href="downloads.php">downloads</a> page for more information on downloading and
+		installing PTP.
 		<li>November 14, 2009: <img src="../images/new.gif"><a href="http://wiki.eclipse.org/PTP/tutorials/SC09">PTP SC09 Tutorial slides</a> posted
 		<li>November 14, 2009: <img src="../images/new.gif"> See <a href="http://wiki.eclipse.org/PTP/meetings/SC09#PTP_.40_SC09">
 		All PTP and Photran-related events at SC09</a>
