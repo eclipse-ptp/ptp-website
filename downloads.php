95208d5e17a689bbaa5639ee5e5bf63b9ad743b5
btibbitts <btibbitts>
2010-10-05 15:20:56 +0000

    update to 4.0.3 info

--- downloads.php
+++ downloads.php
@@ -32,16 +32,22 @@
 
 	<div id="midcolumn">
 		<h1>$pageTitle</h1>
-		<p>Welcome to the downloads page for the Parallel Tools Platform (incuding Photran). The latest release of PTP is <b><font size=+1>4.0.2</font></b></p>
+		<p>Welcome to the downloads page for the Parallel Tools Platform (incuding Photran). The latest release of PTP is <b><font size=+1>4.0.3</font></b></p>
     <p>Please see the <font size=+1><a href="http://wiki.eclipse.org/PTP/release_notes/4.0">
       release notes and installation instructions</a><font size=-1> for prerequisite information 
       and instructions on building and installing PTP Release 4.0.  
 
 <p>
-PTP <b><font size=+1>4.0.0</font></b> is available using the Eclipse Installation Manager from the Helios update site: <a href="http://download.eclipse.org/releases/helios">http://download.eclipse.org/releases/helios</a>
+PTP <b><font size=+1>4.0.3</font></b> is part of the Eclipse Helios (3.6)
+Service Release 1 (SR1), released on September 24, 2010, 
+available using the Eclipse Installation Manager from the Helios update site: 
+<a href="http://download.eclipse.org/releases/helios">http://download.eclipse.org/releases/helios</a>
 </p>
 <p>
-Individual PTP features and  updates (including <b><font size=+1>4.0.2</font></b>) are available from the PTP update site: <a href="http://download.eclipse.org/tools/ptp/updates/helios">http://download.eclipse.org/tools/ptp/updates/helios</a>
+Individual PTP features and  updates (including <b><font size=+1>4.0.3</font></b>) 
+are also available from the PTP update site: 
+<a href="http://download.eclipse.org/tools/ptp/updates/helios">http://download.eclipse.org/tools/ptp/updates/helios</a>
+which at times contains updates more recent that what is available from the Helios update site.
 </p>
 
 <p>Other PTP builds are also available from the <a href="http://wiki.eclipse.org/PTP/builds">PTP builds page</a>.
@@ -49,35 +55,13 @@ Individual PTP features and  updates (including <b><font size=+1>4.0.2</font></b
 
   <div class="homeitem3col">
   <h3>Remote Development Tools (RDT) Server Components</h3>
-  <table>
-  <tr>
-    <td align=left valign=top >
-        <tr bgcolor="#CCCCCC">
-          <th width="34%"  scope="col"><div align="left">Platform</div></th>
-          <th width="10%"  scope="col"><div align="left">Version</div></th>
-          <th width="56%"  scope="col"><div align="left">Download</div></th>
-        </tr>
-        <tr>
-          <td>Linux</td>
-          <td><div align="left">4.0.2</div></td>
-          <td><div align="left"><a href="http://www.eclipse.org/downloads/download.php?file=/tools/ptp/updates/helios/rdt-server-linux-4.0.2.tar">rdt-server-linux-4.0.2.tar</a></div></td>
-        </tr>
-        <tr>
-          <td>Unix</td>
-          <td><div align="left">4.0.2</div></td>
-          <td><div align="left"><a href="http://www.eclipse.org/downloads/download.php?file=/tools/ptp/updates/helios/rdt-server-unix-4.0.2.tar">rdt-server-unix-4.0.2.tar</a></div></td>
-        </tr>
-		<tr>
-          <td>MacOS X</td>
-          <td><div align="left">4.0.2</div></td>
-          <td><div align="left"><a href="http://www.eclipse.org/downloads/download.php?file=/tools/ptp/updates/helios/rdt-server-macosx-4.0.2.tar">rdt-server-macosx-4.0.2.tar</a></div></td>
-        </tr>
-		<tr>
-          <td>Windows</td>
-          <td><div align="left">4.0.2</div></td>
-          <td><div align="left"><a href="http://www.eclipse.org/downloads/download.php?file=/tools/ptp/updates/helios/rdt-server-windows-4.0.2.zip">rdt-server-windows-4.0.2.zip</a></div></td>
-        </tr>
-      </table>   
+  <p>Two Remote Providers work with RDT:
+  <ul>
+  <li>Remote Tools (part of PTP) - the remote server components are moved to the remote target automatically.
+  <li>RSE -   RDT server components need to be separately installed on the remote target
+  if you are using the RSE Remote Provider. See the <a href="http://wiki.eclipse.org/PTP/builds/4.0.3">4.0.3 builds page</a> to download these archives.
+  </ul>
+  
   </div>
       
   
