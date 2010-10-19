c9a18a61b68a04ccbb5874619f3cd2f1f01d14b7
btibbitts <btibbitts>
2010-10-19 20:11:40 +0000

    build a page of tables and links for a PTP/RDT build

---
+++ ptp_rdt_download.php
@@ -0,0 +1,98 @@
+<html>
+<head>
+  <title> PTP RDT Download</title>
+  </head>
+
+<body>
+<?php
+// Beth Tibbitts  tibbitts@us.ibm.com   19 Oct 2010
+// Construct tables and links for the ptp and rdt downloads for a single build
+// sample usage: http://eclipse.org/ptp/ptp_rdt_download.php?parameter=15%20Oct,indigo,I201010160851,5.0.0
+$array = split(",", $_GET{parameter});
+echo "first is ",$array[0]," and second is ",$array[1];
+
+$date=$array[0];
+$eclipseVer=$array[1];
+$buildid=$array[2];
+$rdtVer=$array[3];
+//echo "<p>";
+//echo "<p> date=",$date,"    <br>eclipseVer=",$eclipseVer," <br>buildid=",$buildid," <br>rdtVer=",$rdtVer;
+//echo "<p>";
+?>
+
+<h2>PTP Master archive for build <?php echo "I.",$buildid;?> </h2>
+
+</table><table border="1" cellpadding="5">
+<tr>
+<th> Date
+</th><th> Build
+</th><th> Download Link
+</th>
+</tr>
+<tr>
+
+<?php
+// spit out the name for a ptp-master zip file
+function master($name,$eclipseVer,$buildid,$rdtVer,$suffix=".zip"){
+  $zipname=$name."-".$rdtVer."-".$buildid.$suffix;
+  echo "<a href=\"http://www.eclipse.org/downloads/download.php?file=/tools/ptp/builds/";
+  echo $eclipseVer,"/I.",$buildid,"/",$zipname,"\"";
+  echo " class=\"external text\" title=\"http://www.eclipse.org/downloads/download.php?file=/tools/ptp/builds/",$eclipseVer,"/I.",$buildid,"/",$zipname,"\" rel=\"nofollow\">";
+  echo $zipname,"</a>";
+}
+
+echo "<td>",$date,"</td>";
+echo "<td>","I.",$buildid,"</td>";
+echo "<td>";
+master("ptp-master",$eclipseVer,$buildid,$rdtVer);
+echo "</td></tr></table>";
+?>
+
+<h2>PTP RDT Server archives for build <?php echo "I.",$buildid;?> </h2>
+<?php
+// spit out the url info for an rdt server archive.  name is e.g. "rdt-server-aix"
+function server($name,$eclipseVer,$buildid,$rdtVer,$suffix=".tar"){
+echo "</td><td> <a href=\"http://www.eclipse.org/downloads/download.php?file=/tools/ptp/builds/";
+echo $eclipseVer,"/I.",$buildid,"/",$name,"-",$rdtVer,$suffix,"\"";
+echo " class=\"external text\" title=\"http://www.eclipse.org/downloads/download.php?file=/tools/ptp/builds/",$eclipseVer,"/I.",$buildid,"/",$name,"-",$rdtVer,$suffix,"\" rel=\"nofollow\">";
+echo $name,"</a>";
+}
+?>
+<p>
+</table><table border="1" cellpadding="5">
+<tr>
+<th> Date
+</th><th> Build
+</th><th> AIX
+</th><th> Linux
+</th><th> Linux PPC
+</th><th> Linux x86
+</th><th> MacOS X
+</th><th> Unix
+</th><th> Windows
+</th>
+</tr>
+<tr>
+<?php
+echo "<td>",$date,"</td>";
+echo "<td> I.",$buildid;
+
+server("rdt-server-aix",$eclipseVer,$buildid,$rdtVer);
+server("rdt-server-linux",$eclipseVer,$buildid,$rdtVer);
+server("rdt-server-linux.ppc",$eclipseVer,$buildid,$rdtVer);
+server("rdt-server-linux.x86",$eclipseVer,$buildid,$rdtVer);
+server("rdt-server-macosx",$eclipseVer,$buildid,$rdtVer);
+server("rdt-server-unix",$eclipseVer,$buildid,$rdtVer);
+server("rdt-server-windows",$eclipseVer,$buildid,$rdtVer,".zip");
+
+echo "</td></tr></table>";
+
+echo "<p>&nbsp;";
+echo "<p><a href=\"http://wiki.eclipse.org/PTP/builds/",$rdtVer,"\""," >Back to PTP ",$rdtVer," builds page</a>";
+
+?>
+
+</body>
+
+
+</html?
