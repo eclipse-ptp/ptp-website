39165238e60d8970920179f7b12c083b0396248e
gwatson <gwatson>
2010-11-08 18:25:12 +0000

    updated

--- downloads.php
+++ downloads.php
@@ -52,31 +52,21 @@ The <b><font size=+1>PTP 4.0.4</font></b> release is now available from the PTP 
 
 <div class="homeitem3col">
   <h3>Proxy Agent and Scalable Debug Manager (SDM) Source Code</h3>
-  <p>The IBM LoadLeveler, IBM PE, and SLURM proxy agents, and the SDM need to be built and installed from source code.
-  <table>
-        <tr bgcolor="#CCCCCC">
-          <th width="20%"  scope="col"><div align="left">Version</div></th>
-          <th width="80%"  scope="col"><div align="left">Download</div></th>
-        </tr>
-        <tr>
-          <td><div align="left">4.0.4</div></td>
-          <td><div align="left"><a href="http://www.eclipse.org/downloads/download.php?file=/tools/ptp/updates/helios/ptp-proxy-4.0.4-I201011051906.zip">ptp-proxy-4.0.4-I201011051906.zip</a></div></td>
-        </tr>
-        <tr>
-          <td><div align="left">4.0.3</div></td>
-          <td><div align="left"><a href="http://www.eclipse.org/downloads/download.php?file=/tools/ptp/releases/galileo/ptp-proxy-4.0.4-I201011051906.zip">ptp-proxy-4.0.4-I201011051906.zip</a></div></td>
-        </tr>
-  </table>  
+  <p>The IBM LoadLeveler, IBM PE, and SLURM proxy agents, and the SDM need to be built and installed from source code. The following download link will work for either the 4.0.4 or 4.0.3 (Helios SR1)
+  versions of PTP. See the release notes for instructions on installing the proxy agents.
+  <ul>
+  <li><a href="http://www.eclipse.org/downloads/download.php?file=/tools/ptp/updates/helios/ptp-proxy-4.0.4-I201011051906.zip">ptp-proxy-4.0.4-I201011051906.zip</a>
+  </ul>
 </div>
 
 <div class="homeitem3col">
   <h3>Remote Development Tools (RDT) Server Components</h3>
-  <p>Two Remote Providers work with RDT:
+  <p>There are two remote service providers available that work with RDT:
   <ul>
-  <li>Remote Tools (part of PTP) - the remote server components are moved to the remote target automatically.
-  <li>RSE -   RDT server components need to be separately installed on the remote target
-  if you are using the RSE Remote Provider. See the <a href="http://wiki.eclipse.org/PTP/builds/4.0.4">4.0.4 builds page</a> to download these archives.
+  <li>Remote Tools (installed as part of PTP) - the remote server components are moved to the remote target automatically.
+  <li>Remote System Explorer (separately installed) - RDT server components need to be separately installed on the remote target. See the <a href="http://wiki.eclipse.org/PTP/builds/4.0.4">4.0.4 builds page</a> to download these archives.
   </ul>
+  See the release notes for more information on installing the RSE server components.
 </div>
       
   
