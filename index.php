dbf86344b516c3813134ab736454c70f5f7d031f
gwatson <gwatson>
2012-02-16 17:33:08 +0000

    Remove left nav.

--- index.php
+++ index.php
@@ -104,5 +104,5 @@ EOHTML;
 
 
 	# Generate the web page
-	$App->generatePage($theme, $Menu, $Nav, $pageAuthor, $pageKeywords, $pageTitle, $html);
+	$App->generatePage($theme, $Menu, null, $pageAuthor, $pageKeywords, $pageTitle, $html);
 ?>
