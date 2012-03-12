72d445d497525effdae056fe0cb3b68057c24e49
btibbitts <btibbitts>
2012-03-12 21:05:18 +0000

    506 on top

--- index506.php
+++ index506.php
@@ -80,12 +80,13 @@
 		</div>
 		
 		<div id="headlines">
+			<h3>PTP 5.0.6 Now Available</h3>
+			<p><i>March 12, 2012 -</i> Check the <a href="downloads.php">Download</a> link on how to get yours.</p>
+		
 			<h3>Eclipse IDE for Parallel Application Developers has been updated in SR2</h3>
 			<p><i>February 24, 2012 -</i> PTP 5.0.5 is now part of the 
 			Eclipse Indigo SR2 release. Check the <a href="downloads.php">Download</a> link on how to get yours.</p>
 			
-			<h3>PTP 5.0.6 Now Available</h3>
-			<p><i>March 12, 2012 -</i> Check the <a href="downloads.php">Download</a> link on how to get yours.</p>
 
 		</div>
 		
