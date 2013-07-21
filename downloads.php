a1f143ef069929ecad253b1e2b732508cea26d75
Beth Tibbitts <tibbitts@us.ibm.com>
2013-07-21 10:06:04 -0400

    fix tag

--- downloads.php
+++ downloads.php
@@ -84,7 +84,7 @@ PTP releases automatically.
 
 <div class="homeitem3col">
 <h3>File Downloads</h2>
-<p><b>Archived Update Site, Sysmon Stand-alone System Monitoring, Updated Parallel Package builds, and RDT server components<b>
+<p><b>Archived Update Site, Sysmon Stand-alone System Monitoring, Updated Parallel Package builds, and RDT server components</b>
 <p>Available from <a href="http://download.eclipse.org/tools/ptp/builds/7.0.2">PTP 7.0.2 file downloads</a>:
 <ul>
 <li>Archived update site (ptp-master)
